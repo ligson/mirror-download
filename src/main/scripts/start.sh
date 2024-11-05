8e68a5ba8ea57fac791bdfeb759ec69ddfdc2e83
lijsd <lijsd@yonyou.com>
2024-11-05 19:46:34 +0800

    时区修改

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -9,9 +9,9 @@ for jar in `ls $WORK_HOME/lib/*.jar`
 	do LIB_PATH=$LIB_PATH:$jar
 done
 
-echo "$RUN_JAVA -Dfile.encoding=UTF-8 -classpath $LIB_PATH org.ligson.mirrordownload.AppBoot"
+echo "$RUN_JAVA -Duser.timezone=Asia/Shanghai -Dfile.encoding=UTF-8 -classpath $LIB_PATH org.ligson.mirrordownload.AppBoot"
 #export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:$WORK_HOME/tools/lib'
-nohup $RUN_JAVA -Dfile.encoding=UTF-8 -classpath $LIB_PATH org.ligson.mirrordownload.AppBoot >>mirror-download.log 2>1&
+nohup $RUN_JAVA -Duser.timezone=Asia/Shanghai -Dfile.encoding=UTF-8 -classpath $LIB_PATH org.ligson.mirrordownload.AppBoot >>mirror-download.log 2>1&
 echo $!>mirror-download.pid
 PID=`cat mirror-download.pid`
 echo "启动mirror-download成功,PID:$PID"
